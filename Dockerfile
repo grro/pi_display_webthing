44c3265d12bb9e8d84c87a612448474c605c7d5a
Gregor Roth <gregor.roth@web.de>
2021-01-04 16:08:49 +0100

    hostname removed

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,5 @@
 FROM python:3.9.1-alpine
 
-ENV i2c_expander PCF8574
-ENV i2c_address 0x27
-
 ADD . /tmp/
 WORKDIR /tmp/
 RUN  python /tmp/setup.py install
--- pi_display_webthing/app.py
+++ pi_display_webthing/app.py
@@ -61,7 +61,7 @@ class App(ABC):
         logging.basicConfig(format='%(asctime)s %(name)-20s: %(levelname)-8s %(message)s', level=log_level, datefmt='%Y-%m-%d %H:%M:%S')
 
         if args.command is None:
-            self.print_usage_info(args.hostname, str(args.port))
+            self.print_usage_info(str(args.port))
         elif args.command == 'deregister':
             if args.port is None:
                 self.print_usage_info(str(args.port), "--port is mandatory for deregister command")
