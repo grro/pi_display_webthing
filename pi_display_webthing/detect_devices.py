0c6e457c0e6cee8956f6d307081c71b3c17e42a1
gregor roth <gregor.roth@web.de>
2022-04-04 21:55:15 +0200

    import fixed

--- pi_display_webthing/detect_devices.py
+++ pi_display_webthing/detect_devices.py
@@ -1,11 +1,11 @@
-import smbus
-from typing import List, Optional
+from smbus2 import SMBus
+from typing import List
 
 
 def scan() -> List[str]:
     devices = []
     try:
-        bus = smbus.SMBus(1) # 1 indicates /dev/i2c-1
+        bus = SMBus(1) # 1 indicates /dev/i2c-1
         for device in range(128):
             try:
                 bus.read_byte(device)
@@ -15,3 +15,4 @@ def scan() -> List[str]:
     except FileNotFoundError as e:
         print("WARNING: I2C seems not to be activated")
     return devices
+
